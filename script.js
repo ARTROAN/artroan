edca125d09d4e29e416f74e828e60e28c73f3399
Vanja Veselinovic <vanja.veselinovic.95@gmail.com>
2017-05-05 19:38:53 -0400

    images, scale tiles proportional to screen size, add title

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
+	$(window).resize(function () {
+		$('.page-1 .tiles-group').css('transform', 'scale('+$('body').height()/780+')');
+	}).resize();
+
 	$('.nav-right').hover(function () {
-		$('#tiles-container').css('transform', 'translate(-5px, -8px)')
+		$('#tiles-container').css('transform', 'translate(-5px, -8px)');
 	}, function () {
-		$('#tiles-container').css('transform', 'translate(0, 0)')
+		$('#tiles-container').css('transform', 'translate(0, 0)');
 	});
 });
